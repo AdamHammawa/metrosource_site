3cc15625efe6e171a649c2d1f05c3e62b78238ac
AdamHammawa <adamsani2020@gmail.com>
2023-03-11 01:38:28 +0100

    finalizing

--- assets/js/main.js
+++ assets/js/main.js
@@ -229,15 +229,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Disable using Ctrl on web page
 
-document.addEventListener("keydown", function (event) {
-  if (event.ctrlKey) {
-    event.preventDefault();
-  }
-  if (event.keyCode == 123) {
-    event.preventDefault();
-  }
-});
+// document.addEventListener("keydown", function (event) {
+//   if (event.ctrlKey) {
+//     event.preventDefault();
+//   }
+//   if (event.keyCode == 123) {
+//     event.preventDefault();
+//   }
+// });
 
 // Disable right click
 
-document.addEventListener("contextmenu", (event) => event.preventDefault());
+// document.addEventListener("contextmenu", (event) => event.preventDefault());
